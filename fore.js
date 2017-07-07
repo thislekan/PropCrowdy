e8adf927a26aa73d4b09fd71d3cfd6fa489a35df
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2017-07-07 10:58:45 +0100

    Added Files for PropCrowdy's Homepage

---
+++ fore.js
@@ -0,0 +1,28 @@
+let navbar = document.getElementById("navbar");
+let navlinks = document.querySelectorAll("nav div a");
+console.log(navlinks);
+//console.log(pageYOffset);
+/*window.addEventListener("scroll", () => {
+    var distY = window.pageYOffset||document.documentElement.scrollTop;
+    let shrinkon = 120;
+    if (distY === 300) {
+        console.log("Hello");
+    }
+})*/
+
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 150) {
+        navbar.style.backgroundColor = "white";
+        navbar.style.color = "black";
+        for (var i = 0; i < navlinks.length - 1; i++) {
+            navlinks[i].style.color = "black";
+        }
+    }else{
+        navbar.style.backgroundColor = "transparent";
+        navbar.style.color = "white";
+        for (var i = 0; i < navlinks.length - 1; i++) {
+            navlinks[i].style.color = "white";
+        }
+    }
+})
